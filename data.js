f1a4110c635232928b00f726d0ea193e7defdea0
Neelansh Agrawal <138201009+Neelansh1780@users.noreply.github.com>
2023-09-27 23:36:32 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,35 @@
+//Songs Array containing 5 JS Music Objects
+
+let songs = 
+[
+    {
+        name : 'Apocalypse',
+        path : 'Song/Song 1.mp3',
+        artist : 'Midranger',
+        cover : 'Cover/cover 1.png'
+    },
+    {
+        name : 'Cartoon-On&On',
+        path : 'Song/Song 2.mp3',
+        artist : 'Daniel Levi',
+        cover : 'Cover/cover 2.png'
+    },
+    {
+        name : 'Dogle',
+        path : 'Song/Song 3.mp3',
+        artist : 'SV-The Poet',
+        cover : 'Cover/cover 3.png'
+    },
+    {
+        name : 'Island',
+        path : 'Song/Song 4.mp3',
+        artist : 'MBB & Jonas Schmidt',
+        cover : 'Cover/cover 4.png'
+    },
+    {
+        name : 'Like Me',
+        path : 'Song/Song 5.mp3',
+        artist : 'Britt Lari & T-Mass',
+        cover : 'Cover/cover 5.png'
+    },
+]
